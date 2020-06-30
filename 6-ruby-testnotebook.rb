f30fb4f0c59b50f7985a180dcc02cd9dccee8b54
renjiaxin <renjiaxin@pwrd.com>
2020-06-30 17:23:15 +0800

    字符串的操作

--- 6-ruby-testnotebook.rb
+++ 6-ruby-testnotebook.rb
@@ -9,3 +9,9 @@ puts now
 # 日期
 date = Date.new(2001,2,3).next_year
 puts date
+
+# 字符串的操作
+splitStr = "hello world".split(" ")
+puts(splitStr[0], splitStr[1], splitStr.size)
+test = "I:have:a:dream!".end_with?("!")
+print(test)
