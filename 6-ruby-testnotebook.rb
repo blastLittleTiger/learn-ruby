78fae2a4b267e7bd7348d2143777464143737311
renjiaxin <renjiaxin@pwrd.com>
2020-06-30 17:40:08 +0800

    文件的简单读写

--- 6-ruby-testnotebook.rb
+++ 6-ruby-testnotebook.rb
@@ -1,5 +1,6 @@
-require 'Date'
-require 'Time'
+require 'date'
+require 'time'
+
 # 时间
 time = Time.new(2009)
 puts time
@@ -15,3 +16,10 @@ splitStr = "hello world".split(" ")
 puts(splitStr[0], splitStr[1], splitStr.size)
 test = "I:have:a:dream!".end_with?("!")
 print(test)
+
+# file, 文件的简单读写
+myfile = File.new("D:/1.txt", "r+")
+puts myfile.path
+readlines = myfile.readlines
+puts readlines
+myfile.write(12312313)
